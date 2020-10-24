d76a61d17c0f48f4a6f86128f8f631a7a9d0180b
Hosein <hs.ramezanpoor@gmail.com>
2020-10-24 13:28:14 +0330

    Bump to version 0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 
 setup(name='fixer-demo',
-      version='0.1',
+      version='0.2',
       description='Fixer service demo package',
       url='https://gitlab.com/7learn-py-web/fixer-demo',
       author='Hosein',
       author_email='hosein@inprobes.com',
       license='MIT',
       packages=['fixer'],
+      install_requires=['requests'],
       zip_safe=False)
