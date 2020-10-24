2138613c93698d58080775e981e671f93f2ebd0a
Hosein <hs.ramezanpoor@gmail.com>
2020-10-24 13:09:25 +0330

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fixer-demo',
+      version='0.1',
+      description='Fixer service demo package',
+      url='https://gitlab.com/7learn-py-web/fixer-demo',
+      author='Hosein',
+      author_email='hosein@inprobes.com',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
